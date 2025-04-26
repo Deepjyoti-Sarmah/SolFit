36fa5f04101ce062e06d2f2b060c34a77fb7e3b1
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-04-26 16:10:53 +0530

    adding main.go file

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+
+	"github.com/Deepjyoti-Sarmah/sol-kit-backend/config"
+	"github.com/Deepjyoti-Sarmah/sol-kit-backend/storage"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	// db connection
+	dbConn, err := storage.NewPostgresDb(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer dbConn.Close()
+
+	return nil
+}
