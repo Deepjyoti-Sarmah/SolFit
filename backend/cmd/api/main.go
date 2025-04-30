62fb4db1d157e45b7921478d458448cd7145893e
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-04-30 12:53:54 +0530

    adding server to main

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/Deepjyoti-Sarmah/sol-kit-backend/api"
 	"github.com/Deepjyoti-Sarmah/sol-kit-backend/config"
+	db "github.com/Deepjyoti-Sarmah/sol-kit-backend/internal/models"
 	"github.com/Deepjyoti-Sarmah/sol-kit-backend/storage"
 )
 
@@ -20,11 +22,18 @@ func run() error {
 	}
 
 	// db connection
-	dbConn, err := storage.NewPostgresDb(cfg)
+	dbPool, err := storage.NewPostgresDb(cfg)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	defer dbConn.Close()
+	defer dbPool.Close()
+
+	queries := db.New(dbPool)
+
+	server := api.NewServer(cfg, dbPool, queries)
+	if err := server.Start(); err != nil {
+		return nil
+	}
 
 	return nil
 }
